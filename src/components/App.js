c276bd7e2d7c75e7e19c9cee44d6e28d56026704
Louis Aurick <louisaurick@ntymail.com>
2018-11-06 15:21:19 +0100

    firebase

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,27 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base.js";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentWillMount() {
+    // On modifie que le magasin qu'on obtient via le router
+    const { params } = this.props.match;
+    console.log(`${params.storeId}/fishes`);
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
     // .1 Take a copy of the existing state
